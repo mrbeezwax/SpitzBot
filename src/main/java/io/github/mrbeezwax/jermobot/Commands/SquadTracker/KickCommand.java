3c79d039910f36f96e516370447896b6d7e63148
Juju Liao <liaojulian@gmail.com>
2019-05-23 18:23:25 -0700

    Started kick command

--- src/main/java/io/github/mrbeezwax/jermobot/Commands/SquadTracker/KickCommand.java
+++ src/main/java/io/github/mrbeezwax/jermobot/Commands/SquadTracker/KickCommand.java
@@ -1,4 +1,25 @@
 package io.github.mrbeezwax.jermobot.Commands.SquadTracker;
 
-public class KickCommand {
+import io.github.mrbeezwax.jermobot.Commands.Command;
+import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+
+import java.util.List;
+
+public class KickCommand implements Command {
+    private static final String DESCRIPTION = "Kick a member from your squad";
+
+    @Override
+    public void runCommand(MessageReceivedEvent event, List<String> args) {
+
+    }
+
+    @Override
+    public String getDescription() {
+        return DESCRIPTION;
+    }
+
+    @Override
+    public boolean forAdmin() {
+        return false;
+    }
 }
